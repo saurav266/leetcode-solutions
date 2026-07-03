4792a41e505a0979921148b3243ef9670f49e2aa
Saurav kumar <134755554+saurav266@users.noreply.github.com>
2026-07-03 19:38:04 +0530

    Time: 9 ms (96.31%), Space: 48.6 MB (86.45%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -0,0 +1,54 @@
+class Solution {
+    public class Pair implements Comparable<Pair>{
+        int node;
+        int time;
+        Pair(int node,int time){
+            this.node =node;
+            this.time=time;
+        }
+        public int compareTo(Pair p){
+            if(this.time==p.time) return this.node-p.node;
+            return this.time-p.time;
+        }
+    }
+    public int networkDelayTime(int[][] times, int n, int k) {
+        List<List<Pair>> adj= new ArrayList<>();
+        for(int i=0;i<=n;i++){
+            adj.add(new ArrayList<Pair>());
+        }
+
+        for(int i=0;i<times.length;i++){
+            int u=times[i][0];
+            int v= times[i][1];
+            int time=times[i][2]; 
+            adj.get(u).add(new Pair(v,time));
+        }
+        int[] ans = new int[n+1];
+        Arrays.fill(ans,Integer.MAX_VALUE);
+        ans[k]=0;
+
+        PriorityQueue<Pair> pq= new PriorityQueue<>();
+        pq.add(new Pair(k,0));
+
+        while(pq.size()>0){
+            Pair top= pq.remove();
+            int node =top.node;
+            int time=top.time;
+            if(top.time> ans[node]) continue;
+            for(Pair p:adj.get(node)){
+                int totaltime=top.time+ p.time;
+                if(totaltime<ans[p.node]){
+                    ans[p.node]=totaltime;
+                    pq.add(new Pair(p.node,totaltime));
+                }
+            }
+        }
+
+        int max=-1;
+        for(int i=1;i<=n;i++){
+            if(ans[i]==Integer.MAX_VALUE) return -1;
+            max= Math.max(max,ans[i]);
+        }
+        return max;
+    }
+}
