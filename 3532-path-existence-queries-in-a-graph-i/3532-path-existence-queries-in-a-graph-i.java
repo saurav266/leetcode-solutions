30675350a2af6ba4886c0b7788a2f9a20b291013
Saurav kumar <134755554+saurav266@users.noreply.github.com>
2026-07-10 07:56:52 +0530

    Time: 67 ms (5.83%), Space: 210.5 MB (5.83%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
@@ -1,25 +1,49 @@
 class Solution {
-    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
-        int[] component = new int[n];
+    public void bfs(int start, int compId,List<List<Integer>> adj, int[] comp){
+        comp[start]= compId;
+        Queue<Integer> q= new LinkedList<>();
+        q.add(start);
+        while(q.size()>0){
+            int node= q.poll();
 
-        int val = 0;
-        component[0] = 0;
-
-        for (int i = 1; i < n; i++) {
-            if (nums[i] - nums[i - 1] > maxDiff) {
-                val++;
+            for(int nie : adj.get(node)){
+                if(comp[nie]==-1){
+                    comp[nie]= compId;
+                    q.add(nie);
+                }
             }
-            component[i] = val;
+        }
+    }
+    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        List<List<Integer>> adj= new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
         }
 
-        boolean[] ans = new boolean[queries.length];
+        for(int i=0;i<n-1;i++){
+            if(nums[i+1]-nums[i]<=maxDiff){
+                adj.get(i).add(i+1);
+                adj.get(i+1).add(i);
+            }
+        }
+        int[] comp = new int[n];
+        Arrays.fill(comp,-1);
+        int compId=0;
 
-        for (int i = 0; i < queries.length; i++) {
-            int u = queries[i][0];
-            int v = queries[i][1];
-            ans[i] = component[u] == component[v];
+        for(int i=0;i<n;i++){
+            if(comp[i]==-1){
+                bfs(i,compId,adj,comp);
+                compId++;
+            }
         }
 
+        boolean[] ans= new boolean[queries.length];
+        for(int i=0;i<queries.length;i++){
+            int u = queries[i][0], v = queries[i][1];
+            ans[i] = (comp[u] == comp[v]);
+        }
         return ans;
     }
+
 }
