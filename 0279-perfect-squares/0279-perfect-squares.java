8d6b97f1231c6dc761ff1c21e081ede34734865f
Saurav kumar <134755554+saurav266@users.noreply.github.com>
2026-07-15 07:18:07 +0530

    Time: 25 ms (81.07%), Space: 44.4 MB (76.19%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,23 +1,24 @@
 class Solution {
     public boolean isPerfect(int n){
-        int sqrt=(int)(Math.sqrt(n));
+        int sqrt= (int)(Math.sqrt(n));
 
-        return (sqrt* sqrt==n);
+        return (sqrt* sqrt ==n);
     }
-    public int helper(int n,int[] dp) {
-        if(isPerfect(n)) return 1;
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        for(int i=1;i<=n;i++){
+            if(isPerfect(i)) dp[i]=1;
+            else {
+                int min=Integer.MAX_VALUE;
 
-        if(dp[n]!=-1) return dp[n];
-        int min= Integer.MAX_VALUE;
-        for(int i=1;i*i<n;i++){
-            int count= helper(i*i,dp) + helper(n-i*i,dp);
-            min= Math.min(count,min);
+                for(int j=1;j*j<=i;j++){
+                    int count= dp[j*j] + dp[i-j*j];
+                    min= Math.min(min,count);
+                }
+                dp[i]= min;
+            }
+
         }
-        return dp[n]= min;
-    }
-    public int numSquares(int n){
-        int[] dp= new int[n+1];
-        Arrays.fill(dp,-1);
-        return helper(n,dp);
+        return dp[n];
     }
 }
