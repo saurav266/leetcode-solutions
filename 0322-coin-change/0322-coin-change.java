e5a775d8099c0abbd945f693def8908dd8c7a1ed
Saurav kumar <134755554+saurav266@users.noreply.github.com>
2026-07-16 08:02:16 +0530

    Time: 37 ms (10.19%), Space: 48.3 MB (5.38%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,19 +1,31 @@
-import java.util.*;
-
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1); // large value
-        dp[0] = 0;
+    public long helper(int i, int[] coins, int amount, long[][] dp){
 
-        for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (i - coin >= 0) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-                }
-            }
+        if(i==coins.length){
+            if(amount==0) return 0;
+            else return Integer.MAX_VALUE;
         }
 
-        return dp[amount] > amount ? -1 : dp[amount];
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        long skip=helper(i+1,coins,amount,dp);
+        if(amount-coins[i]<0) return dp[i][amount] =skip;
+
+        long pick = 1 + helper(i,coins,amount-coins[i],dp);
+
+        return dp[i][amount]= Math.min(skip,pick);
+    }
+    public int coinChange(int[] coins, int amount) {
+        int n= coins.length;
+        long[][] dp= new long[n][amount+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j]=-1;
+            }
+        }
+        int ans=(int) helper(0,coins,amount,dp);
+        if(ans==Integer.MAX_VALUE){
+            return -1;
+        }
+        return ans;
     }
 }
