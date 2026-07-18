923d39b5491501084dc556fd22bf7d36a44e006f
Saurav kumar <134755554+saurav266@users.noreply.github.com>
2026-07-18 06:21:48 +0530

    Time: 61 ms (20.85%), Space: 64 MB (45.35%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -0,0 +1,35 @@
+class Solution {
+     public int lcs(int i, int j, StringBuilder a, StringBuilder b,int[][] dp){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(a.charAt(i)==b.charAt(j)){
+            return dp[i][j]= 1 + lcs(i-1,j-1,a,b,dp);
+        }
+        else{
+            return dp[i][j]= Math.max(lcs(i-1,j,a,b,dp), lcs(i,j-1,a,b,dp));
+        }
+    }
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m=text1.length();
+        int n=text2.length();
+        StringBuilder a= new StringBuilder(text1);
+        StringBuilder b=new StringBuilder(text2);
+       int[][] dp= new int[m][n];
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j]=-1;
+            }
+        }
+        return lcs(m-1,n-1,a,b,dp);
+
+    }
+    public String reverse(String s){
+        StringBuilder sb= new StringBuilder(s);
+        sb.reverse();
+        s=sb.toString();
+        return s;
+    }
+    public int longestPalindromeSubseq(String s) {
+        return longestCommonSubsequence(s, reverse(s));
+    }
+}
