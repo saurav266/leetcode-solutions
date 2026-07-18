4c3349636dfb91b746fc8c30fb0f3ae7ba77fca5
Saurav kumar <134755554+saurav266@users.noreply.github.com>
2026-07-18 07:55:57 +0530

    Time: 1 ms (87.59%), Space: 47.4 MB (44.1%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,16 +1,17 @@
 class Solution {
-    public void bfs(int i, boolean[] vis, int[][] adj){
+    public void bfs(int i,int[][] adj, boolean[] vis){
         int n= adj.length;
         vis[i]=true;
         Queue<Integer> q= new LinkedList<>();
+
         q.add(i);
         while(q.size()>0){
-            int top= q.remove();
+            int top=q.remove();
 
             for(int j=0;j<n;j++){
                 if(adj[top][j]==1 && vis[j]==false){
-                    q.add(j);
                     vis[j]=true;
+                    q.add(j);
                 }
             }
         }
@@ -19,9 +20,10 @@ class Solution {
         int n= adj.length;
         boolean[] vis= new boolean[n];
         int count=0;
+
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                bfs(i,vis,adj);
+            if(vis[i]==false){
+                bfs(i,adj,vis);
                 count++;
             }
         }
