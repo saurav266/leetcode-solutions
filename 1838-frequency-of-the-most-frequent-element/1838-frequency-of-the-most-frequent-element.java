679eed639bfcaf323e0abd7eb7b8c9cede0ef552
Saurav kumar <134755554+saurav266@users.noreply.github.com>
2026-07-25 12:22:56 +0530

    Time: 33 ms (92.54%), Space: 94.5 MB (94.34%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -1,40 +1,22 @@
 class Solution {
-    public int binary(int idx, int k, int[] nums, int[] pref){
-        int target= nums[idx];
-        int i=0;
-        int j=idx;
-        int best_idx=idx;
-        while(i<=j){
-            int mid= i +(j-i)/2;
-
-            int count=(idx-mid+1);
-            int windoSum=count*target;
-
-            int currSum=pref[idx]-pref[mid] +nums[mid];
-
-            int ops= windoSum-currSum;
-
-            if(ops>k){
-                i=mid+1;
-            }
-            else{
-                best_idx=mid;
-                j=mid-1;
-            }
-        }
-        return idx-best_idx +1;
-    }
     public int maxFrequency(int[] nums, int k) {
-        int n= nums.length;
         Arrays.sort(nums);
-        int[] pref= new int[n];
-        pref[0]=nums[0];
-        for(int i=1;i<n;i++){
-            pref[i]= pref[i-1]+nums[i];
-        }
+        int n= nums.length;
+
         int res=0;
-        for(int target_idx=0; target_idx<n;target_idx++){
-            res = Math.max(res,binary(target_idx,k,nums,pref));
+
+        int i=0;
+        long currSum=0;
+        for(int j=0;j<n;j++){
+            long target=nums[j];
+
+            currSum +=nums[j];
+
+            if((j-i+1)* target-currSum >k){
+                currSum -=nums[i];
+                i++;
+            }
+            res=Math.max(res,j-i+1);
         }
         return res;
     }
