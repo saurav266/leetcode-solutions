24d3cfb3afc61d854a4f37849b2fba713a5600c3
Saurav kumar <134755554+saurav266@users.noreply.github.com>
2026-08-11 17:45:51 +0530

    Time: 15 ms (57.9%), Space: 47.2 MB (46.2%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,35 +1,32 @@
 class Solution {
     public String minWindow(String s, String t) {
-       if (s.length() < t.length()) return "";
-       int m=s.length();
-       int n=t.length();
-       Map<Character,Integer> mp= new HashMap<>();
-       for (char ch : t.toCharArray()) {
-            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+        if (s.length() < t.length()) return "";
+        int m= s.length();
+        int n=t.length();
+        Map<Character,Integer> mp= new HashMap<>();
+        for(char ch: t.toCharArray()){
+            mp.put(ch,mp.getOrDefault(ch,0)+1);
         }
-       int minLen = Integer.MAX_VALUE;
-       int countR=n;
-       int start = 0;
-       int i=0;
-       int j=0;
-       while(j<m){
-          char ch=s.charAt(j);
-          if(mp.containsKey(ch)){
-              int freq= mp.get(ch);
+        int minLen=Integer.MAX_VALUE;
+        int countR=n;
+        int start = 0;
+        int i=0;
+        int j=0;
+        while(j<m){
+            char ch= s.charAt(j);
+
+            if(mp.containsKey(ch)){
+                int freq=mp.get(ch);
                 if(freq>0){
-                
                     countR--;
                 }
-             mp.put(ch,mp.get(ch)-1);
-
+                mp.put(ch,mp.get(ch)-1);
             }
-
             while(countR==0){
-                if(j-i+1<minLen){
+                if(minLen> j-i+1){
                     minLen=j-i+1;
                     start=i;
                 }
-
                 char left=s.charAt(i);
                 if(mp.containsKey(left)){
                     mp.put(left,mp.get(left)+1);
@@ -38,9 +35,10 @@ class Solution {
                     }
                 }
                 i++;
+
             }
             j++;
-       }
-       return minLen==Integer.MAX_VALUE ? "" : s.substring(start,start+minLen);
+        }
+        return minLen==Integer.MAX_VALUE ? "" : s.substring(start,start+minLen);
     }
 }
