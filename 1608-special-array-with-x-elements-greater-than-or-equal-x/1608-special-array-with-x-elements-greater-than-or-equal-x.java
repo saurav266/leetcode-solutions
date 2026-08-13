93964819c05e453473edc95adaa49884b3d12779
Saurav kumar <134755554+saurav266@users.noreply.github.com>
2026-08-13 07:46:15 +0530

    Time: 4 ms (33.68%), Space: 43.2 MB (58.42%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,27 +1,15 @@
 class Solution {
-    public int binarySearch(int[] nums, int t){
-        int n= nums.length;
-        int lo=0;
-        int hi=n-1;
-        while(lo<=hi){
-            int mid = lo + (hi-lo)/2;
-            if(nums[mid]<t){
-                lo=mid+1;
-            }
-            else{
-                hi=mid-1;
-            }
-        }
-        return n-lo;
-    }
     public int specialArray(int[] nums) {
         int n= nums.length;
         Arrays.sort(nums);
-        for(int x=0;x<=n;x++){
-            int count=binarySearch(nums,x);
-            if(count==x) return x;
-        }
+        for(int i=0;i<n;i++){
+            int x= n-i;
 
+            if(nums[i]>=x){
+
+                if(i==0|| nums[i-1]<x) return x;
+            }
+        }
         return -1;
     }
 }
