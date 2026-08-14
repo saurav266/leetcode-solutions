ec385fc12bbc90a3b5be50e3eabc22880ebd8c7d
Saurav kumar <134755554+saurav266@users.noreply.github.com>
2026-08-14 08:28:28 +0530

    Time: 25 ms (38.5%), Space: 47.9 MB (93.75%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -2,29 +2,40 @@ class Solution {
     public int maxProfitAssignment(int[] d, int[] p, int[] w) {
         int m= w.length;
         int n=p.length;
-        PriorityQueue<int[]> pq= new PriorityQueue<>((a,b)-> b[0]-a[0]);
-
+        List<int[]> temp= new ArrayList<>();
         for(int i=0;i<n;i++){
-            pq.offer(new int[]{p[i],d[i]});
+            temp.add(new int[]{d[i],p[i]});
         }
-        Arrays.sort(w);
-        for (int i = 0; i < m / 2; i++) {
-            int temp = w[i];
-            w[i] = w[m - 1 - i];
-            w[m - 1 - i] = temp;
+        // sort the list accordingt to the difficulty
+        Collections.sort(temp,(a,b)-> Integer.compare(a[0], b[0]));
+        // Pre-processing to find the maximum profit till index i at constant time
+
+        for(int i=1;i<temp.size();i++){
+            temp.get(i)[1]=Math.max(temp.get(i-1)[1],temp.get(i)[1]);
         }
 
+        //Arrays.sort(w);
         int totalProfit=0;
 
-        int i=0;
-        while(i<m && !pq.isEmpty()){
-            while(!pq.isEmpty() && pq.peek()[1]> w[i]){
-                pq.poll();
+        for(int i=0;i<m;i++){
+            int level=w[i];
+
+            int l=0;
+            int r= temp.size()-1;
+            int maxProfit=0;
+            while(l<=r){
+                int mid = l + (r-l)/2;
+
+                if(temp.get(mid)[0]<=level){
+                    maxProfit=Math.max(maxProfit,temp.get(mid)[1]);
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
+                }
+
             }
-            if(!pq.isEmpty()){
-                totalProfit +=pq.peek()[0];
-            }
-            i++;
+            totalProfit +=maxProfit;
         }
         return totalProfit;
     }
